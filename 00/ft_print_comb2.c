f3478a7b9acc2af6e6c572c3ab551c87584d4b47
Max_ <savenkov-sapsan@mail.ru>
2021-01-13 13:17:27 +0700

    putnbr

---
+++ 00/ft_print_comb2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void ft_putchar(char c);
+
+void ft_print_comb2() {
+    printf("%d", 15 / 10);
+}
--- 00/main.c
+++ 00/main.c
@@ -1,13 +1,16 @@
 
 // void ft_is_negative(int i);
 void ft_putchar(char c);
-void ft_print_comb();
+// // void ft_print_comb();
+// void ft_print_comb2();
+void ft_putnbr(int nb);
 
 int main(void) {
     // ft_putchar('s');
     // ft_is_negative(-1);
-    ft_print_comb();
-    
+    // ft_print_comb();
+    // ft_print_comb2();
+    ft_putnbr(-100000002);
     
     
     
