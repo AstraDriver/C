631843ff487f359e7b250451721258094fb8a17b
AstraDriver <savenkov-sapsan@mail.ru>
2021-03-11 21:30:17 +0700

    ft_atoi_base refactor

--- 03/ft_strstr.c
+++ 03/ft_strstr.c
@@ -13,8 +13,7 @@ char *ft_strstr(char *str, char *to_find) {
             if (j == 0) {
                 k = i;
             }
-            if (to_find[j + 1] == '\0')
-            {
+            if (to_find[j + 1] == '\0') {
                 return (&str[k]);
             }
             j++;
--- 04/ft_atoi_base.c
+++ 04/ft_atoi_base.c
@@ -3,7 +3,6 @@
 
 int ft_strlen(char *str){
     int i = 0;
-
     while (str[i]){
         i++;
     }
@@ -11,15 +10,6 @@ int ft_strlen(char *str){
 }
 
 int ft_strstr(char *str, char to_find) {
-
-    while (*str) {
-        if (*str++ == to_find)
-            return (1);
-    }
-    return (0);
-}
-
-int ft_strstr_basa(char *str, char to_find) {
     int i = 0;
     while (str[i]) {
         if (str[i] == to_find)
@@ -31,64 +21,49 @@ int ft_strstr_basa(char *str, char to_find) {
 
 int ft_base_is_valid(char *base){
     int i = 0;
-
     while (base[i]){
         if (base[i] < 32 || base[i] > 126 || base[i] == '+' || base[i] == '-')
             return 1;
-        if (ft_strstr(&base[i + 1], base[i]) || !base[i + 1])                        // Переделать
+        if (ft_strstr(&base[i + 1], base[i]))                       
             return 1;    
         i++;
     }
+    if (i < 2)
+        return 1;
     return 0;
+}
+
+int		ft_recursive_power(int nb, int power)
+{
+	if (power == 0)
+		nb = 1;
+	if (power < 0)
+		nb = 0;
+	if (power-- > 1)
+		nb = nb * ft_recursive_power(nb, power);
+	return (nb);
 }
 
 int		ft_atoi_base(char *str, char *base) {
 
     int i = 0;
-    int start_num = 0;
-    int power = 0;
+    int first_num = 0;
+    int len_str = 0;
     int result = 0;
     int minus = 1;
-    int pow;
 
     while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n')
-		|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r')){
+		|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r'))
         i++;
-    }
-    while (str[i] == '-' || str[i] == '+'){
-        if (str[i] == '-')
-            minus *= -1;
+    while (str[i] == '-' || str[i] == '+')
+        str[i++] == '-' ? minus *= -1 : 1;
+    first_num = i;
+    while (ft_strstr(base, str[i]))
         i++;
-    }
-
-
-    start_num = i;
-    while (ft_strstr(base, str[i])){
-        i++;
-    }
-    power = i - start_num - 1;
-
-    if (power > 1) {                                   
-        pow = ft_strlen(base);                           
-        printf("%d\n", power);
-        while (power > 1){                             
-            while (power-- > 1)                    
-                pow *= ft_strlen(base);                
-            result = result + (ft_strstr_basa(base, str[start_num++])) * pow;   
-            pow = ft_strlen(base);                                              
-            power = i - start_num - 1;                                 
-        }
-    }
-    if (power == 1){    
-        result = result + (ft_strstr_basa(base, str[start_num++])) * ft_strlen(base);  
-        result += (ft_strstr_basa(base, str[start_num]));                                   
-    }
-    if (power == 0){
-        result = result + ft_strstr_basa(base, str[start_num++]);
-    }
-    return (result * minus);
-
-
-
-
+    len_str = i - first_num;
+    while (len_str >= 1){              
+        result += ft_strstr(base, str[first_num++]) * ft_recursive_power(ft_strlen(base), len_str - 1);
+        len_str--;
+    }                                                                      
+    return ((minus >= 0) ? result : -result);
 }
